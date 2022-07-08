63cecf623e15b4ccfd2c45cea3468f66aa3889eb
Alexei Kolola <repoacc@protonmail.com>
2022-07-08 21:38:20 +0200

    Added: RecyclerView.

--- app/src/main/java/com/example/climateduels/team/TeamFragment.java
+++ app/src/main/java/com/example/climateduels/team/TeamFragment.java
@@ -1,16 +1,22 @@
 package com.example.climateduels.team;
 
+import static com.example.climateduels.R.id.recycler_menu;
+
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.climateduels.R;
 
@@ -18,15 +24,27 @@ public class TeamFragment extends Fragment {
 
     private TeamViewModel mViewModel;
 
-    public static TeamFragment newInstance() {
-        return new TeamFragment();
-    }
+    private RecyclerView recyclerView;
+
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_team, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_team, null);
+        //do whatever
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //final TextView message = view.findViewById(R.id.message);
+        //message.setText("Test");
+
+        recyclerView = view.findViewById(recycler_menu);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new CustomAdapter(new String[]{"a", "b", "c"}));
     }
 
     @Override
