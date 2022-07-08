31734eb766084df006cfcca7ad0bb876f9917663
EmmaSophieReichert <emmasophie1203@gmx.de>
2022-07-08 12:30:53 +0200

    StartActivity
    xml and java class for StartActivity, connection to database is still missing

---
+++ app/src/main/java/com/example/climateduels/StartActivity.java
@@ -0,0 +1,59 @@
+package com.example.climateduels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.climateduels.dataManager.DataManager;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button submitButton;
+    private EditText
+            teamCodeEdit,
+            userNameEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initUI();
+    }
+
+    private void initUI(){
+        setContentView(R.layout.activity_start);
+        submitButton = findViewById(R.id.start_button_submit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSubmitButtonClicked();
+            }
+        });
+        teamCodeEdit = findViewById(R.id.edit_text_team_name);
+        userNameEdit = findViewById(R.id.edit_text_user_name);
+    }
+
+    private void onSubmitButtonClicked(){
+        String
+                teamCode = teamCodeEdit.getText().toString(),
+                userName = userNameEdit.getText().toString();
+        /**
+        if(DataManager.containsTeam(teamCode)) {
+
+        }
+        else {
+
+        }
+         */
+
+    }
+
+    private void startCategoryChooserActivity() {
+
+    }
+
+
+}
